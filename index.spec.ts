63f2ec3e95413c1cd35c41ead95983ef86e8e535
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-11-27 14:09:44 +0100

    Add pack function

--- index.spec.ts
+++ index.spec.ts
@@ -1,9 +1,9 @@
 import { test } from "ava";
-import { LuaArray, lualength, tonumber } from "./index";
+import { LuaArray, lualength, pack, tonumber } from "./index";
 
-test("length of a two items array", t => {
+test("length of a two items array", (t) => {
     // Arrange
-    const array:LuaArray<string> = { 1: "first", 2: "second" };
+    const array: LuaArray<string> = { 1: "first", 2: "second" };
 
     // Act
     const result = lualength(array);
@@ -12,9 +12,9 @@ test("length of a two items array", t => {
     t.is(result, 2);
 });
 
-test("length of a two items array with a hole", t => {
+test("length of a two items array with a hole", (t) => {
     // Arrange
-    const array:LuaArray<string> = { 1: "first", 3: "second" };
+    const array: LuaArray<string> = { 1: "first", 3: "second" };
 
     // Act
     const result = lualength(array);
@@ -23,10 +23,9 @@ test("length of a two items array with a hole", t => {
     t.is(result, 1);
 });
 
-
-test("length of an empty array", t => {
+test("length of an empty array", (t) => {
     // Arrange
-    const array:LuaArray<string> = {};
+    const array: LuaArray<string> = {};
 
     // Act
     const result = lualength(array);
@@ -35,28 +34,34 @@ test("length of an empty array", t => {
     t.is(result, 0);
 });
 
-test("tonumber of a number returns a number", t => {
+test("tonumber of a number returns a number", (t) => {
     // Act
     const result = tonumber("18");
 
     // Assert
     t.is(result, 18);
-})
+});
 
-
-test("tonumber of a decimal number returns a number", t => {
+test("tonumber of a decimal number returns a number", (t) => {
     // Act
     const result = tonumber("18.5");
 
     // Assert
     t.is(result, 18.5);
-})
+});
 
-
-test("tonumber of a string returns 0", t => {
+test("tonumber of a string returns 0", (t) => {
     // Act
     const result = tonumber("toto");
 
     // Assert
     t.is(result, 0);
-})
+});
+
+test("pack", (t) => {
+    // Act
+    const result = pack(3, 4, 5);
+
+    // Assert
+    t.deepEqual(result, { 1: 3, 2: 4, 3: 5 });
+});
--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 export interface LuaMap<TKey, TValue> {
-    next(control:number):[TKey, TValue];
+    next(control: number): [TKey, TValue];
 }
 
 export interface LuaArray<T> {
-    [key:number]:T;
+    [key: number]: T;
 }
 
 export interface LuaObj<T> {
-    [key: string]:T;
+    [key: string]: T;
 }
 
-export function ipairs<T>(a:LuaArray<T>) {
-    const pairs:[number, T][] = [];
+export function ipairs<T>(a: LuaArray<T>) {
+    const pairs: [number, T][] = [];
     for (let i = 1; ; i++) {
         if (!a[i]) break;
         pairs.push([i, a[i]]);
@@ -23,7 +23,7 @@ export function pairs<T, TValue>(a: LuaMap<T, TValue>): [T, TValue][];
 export function pairs<T>(a: LuaObj<T>): [string, Diff<T, undefined>][];
 export function pairs<T>(a: LuaArray<T>): [number, Diff<T, undefined>][];
 export function pairs<T>(a: LuaObj<T>): [keyof typeof a, T][] {
-    const pairs:[keyof typeof a, T][] = [];
+    const pairs: [keyof typeof a, T][] = [];
     for (let k in a) {
         pairs.push([k, a[k]]);
     }
@@ -31,22 +31,27 @@ export function pairs<T>(a: LuaObj<T>): [keyof typeof a, T][] {
 }
 
 declare type Diff<T, U> = T extends U ? never : T;
-export function kpairs<T>(a: T): [keyof typeof a, Diff<T[keyof typeof a], undefined>][] {
-    const pairs:[Extract<keyof typeof a, string>, Diff<T[keyof typeof a], undefined>][] = [];
+export function kpairs<T>(
+    a: T
+): [keyof typeof a, Diff<T[keyof typeof a], undefined>][] {
+    const pairs: [
+        Extract<keyof typeof a, string>,
+        Diff<T[keyof typeof a], undefined>
+    ][] = [];
     for (let k in a) {
         pairs.push([k, a[k] as any]);
     }
     return pairs;
 }
 
-export function next<T>(a:T) {
+export function next<T>(a: T) {
     for (let k in a) {
         return [k, a[k]];
     }
     return undefined;
 }
 
-export function tonumber(a: any):number {
+export function tonumber(a: any): number {
     const result = parseFloat(a);
     return isNaN(result) ? 0 : result;
 }
@@ -55,20 +60,22 @@ export function tostring(s: any): string {
     return s.toString();
 }
 
-export function type(a: any) : "table" | "number" | "string" | "function" | "boolean" | undefined {
-    if (typeof(a) === "number") {
+export function type(
+    a: any
+): "table" | "number" | "string" | "function" | "boolean" | undefined {
+    if (typeof a === "number") {
         return "number";
     }
-    if (typeof(a) === "string") {
+    if (typeof a === "string") {
         return "string";
     }
-    if (typeof(a) === "function") {
+    if (typeof a === "function") {
         return "function";
     }
-    if (typeof(a) === "boolean") {
+    if (typeof a === "boolean") {
         return "boolean";
     }
-    if (typeof(a) === "undefined") {
+    if (typeof a === "undefined") {
         return undefined;
     }
     return "table";
@@ -90,59 +97,70 @@ export function assert(condition: any, message?: string) {
     }
 }
 
-export function unpack<T>(t:LuaArray<T>, first?:number, count?:number):(T|undefined)[] {
-    const ret:(T|undefined)[] = [];
-    for(let i = first || 1;; i++) {
+export function unpack<T>(
+    t: LuaArray<T>,
+    first?: number,
+    count?: number
+): (T | undefined)[] {
+    const ret: (T | undefined)[] = [];
+    for (let i = first || 1; ; i++) {
         if (count === undefined) {
             if (t[i] === undefined) break;
-        }
-        else {
+        } else {
             if (count == 0) break;
-            count --;
-        }        
+            count--;
+        }
         ret.push(t[i]);
     }
     return ret;
 }
 
-export function tostringall(...text: object[]){
-    return text.map(x => x === undefined ? "nil" : x.toString());
+export function tostringall(...text: object[]) {
+    return text.map((x) => (x === undefined ? "nil" : x.toString()));
 }
 
 export function select<T>(index: "#", t: T[]): number;
 export function select<T>(index: number, t: T[]): T;
-export function select<T>(index: number|"#", t: T[]): T|number{
+export function select<T>(index: number | "#", t: T[]): T | number {
     if (index == "#") return t.length;
     return t[index - 1];
 }
 
-export function strjoin(separator: string, ...text:string[]) {
+export function strjoin(separator: string, ...text: string[]) {
     return text.join(separator);
 }
 
-export function hooksecurefunc(table:any, methodName:string, hook:(...args:any[])=>void) {
-}
+export function hooksecurefunc(
+    table: any,
+    methodName: string,
+    hook: (...args: any[]) => void
+) {}
 
-export function error(error:string, info:number):void{}
-export function rawset(table: any, key:string, value:any){}
-export function setmetatable<T extends object>(table: T, metatable: { __index?: (o:T, key:string) => any}):T { 
+export function error(error: string, info: number): void {}
+export function rawset(table: any, key: string, value: any) {}
+export function setmetatable<T extends object>(
+    table: T,
+    metatable: { __index?: (o: T, key: string) => any }
+): T {
     const index = metatable.__index;
     if (index) {
         const handler = {
             get: (target: T, key: keyof T) => {
                 return key in target ? target[key] : index(target, <string>key);
-            }
+            },
         };
         return new Proxy(table, handler);
     }
     return table;
 }
-export function loadstring(t: string):() => any { throw Error("Not implemented") }
+export function loadstring(t: string): () => any {
+    throw Error("Not implemented");
+}
 
-export function lualength<T>(array: (LuaArray<T>|string)):number {
-    if (typeof (array) === "string") return array.length;
+export function lualength<T>(array: LuaArray<T> | string): number {
+    if (typeof array === "string") return array.length;
     let length = 0;
-    for (let i = 1; ; i++){
+    for (let i = 1; ; i++) {
         if (array[i] === undefined) {
             length = i - 1;
             break;
@@ -155,4 +173,15 @@ export function truthy<T>(a: T[]) {
     return a.length > 0;
 }
 
-export const _G:any = {};
+type ArrayElement<T extends any[]> = T extends (infer U)[] ? U : never;
+
+export function pack<T extends any[]>(...args: T): LuaArray<ArrayElement<T>> {
+    let i = 1;
+    let result: LuaArray<ArrayElement<T>> = {};
+    for (const k of args) {
+        result[i++] = k;
+    }
+    return result;
+}
+
+export const _G: any = {};
